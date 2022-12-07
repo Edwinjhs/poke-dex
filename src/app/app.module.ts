5af3fa6589aea1852d982ee904c520d5a64b9b44
Edwin Hernandez <edwin.ejhnsn@gmail.com>
2022-12-06 23:29:06 -0500

    my team ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonesComponent } from './components/pokemones/pokemones.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MyteamComponent } from './components/myteam/myteam.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonesComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    MyteamComponent
   ],
   imports: [
     BrowserModule,
